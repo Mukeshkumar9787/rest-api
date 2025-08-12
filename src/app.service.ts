a1a6893e2e3bbbceb9d034b4dc1654013e4c63a8
Mukeshkumar <mukeshkumar87086@gmail.com>
2025-08-12 18:36:10 +0530

    format:

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { timestamp } from 'rxjs';
 
 @Injectable()
 export class AppService {
   getHealthStatus() {
-    return { status: 'ok', timestamp: new Date()};
+    return { status: 'ok', timestamp: new Date() };
   }
 }
